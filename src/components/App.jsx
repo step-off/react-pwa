118066614ef9e177865342f27eaeb6228e0f9177
step-off <nofantasy12@gmail.com>
2019-01-21 12:16:31 +0500

    main interface to get/post data

---
+++ src/components/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export default class App extends React.Component {
+	usersUrl = 'https://jsonplaceholder.typicode.com/users';
+	state = {
+		users: []
+	};
+
+	render() {
+		return (
+			<>
+				<h1>
+					Hello World
+				</h1>
+				<ul>
+					{this.state.users.map(user => <li key={user.id}>{user.name}</li>)}
+				</ul>
+				<div>
+					<button type={'button'} onClick={this.postUsers}>
+						Post users
+					</button>
+				</div>
+			</>
+		)
+	}
+
+	async componentDidMount() {
+		const fetchedUsers = await this.fetchUsers();
+
+		this.setState({
+			users: fetchedUsers
+		});
+	}
+
+	fetchUsers = async () => {
+		const response = await fetch(this.usersUrl);
+
+		return response.json();
+	};
+
+	postUsers = async () => {
+		await fetch(this.usersUrl, {
+			method: 'POST',
+			body: JSON.stringify({
+				username: 'Random User'
+			}),
+			headers: {
+				"Content-type": "application/json; charset=UTF-8"
+			}
+		})
+	};
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import '@babel/polyfill';
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-import App from './components/App';
+import App from './components/App.jsx';
 
 ReactDOM.render(
     React.createElement(App, {}),
