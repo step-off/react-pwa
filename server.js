970fb3d98d3ee9cb25949378c65b8831666dfda5
step-off <nofantasy12@gmail.com>
2019-01-22 15:41:03 +0500

    paths fix

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-	res.sendFile( __dirname + "/" + "index.html" );
+	res.sendFile("index.html" );
 });
 
 app.listen(process.env.PORT || 3000);
